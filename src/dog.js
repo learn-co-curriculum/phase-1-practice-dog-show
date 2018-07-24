1adeb3249b091c8f476fc324aeac39aebe7cf78c
nikymorg <nicole.morgan@gmail.com>
2018-07-24 17:09:05 +0100

    working through patch

--- src/dog.js
+++ src/dog.js
@@ -1,6 +1,26 @@
 class Dog {
-  
-  render() {
-    return `<tr><td>*Name*</td> <td>*Breed*</td> <td>*Sex*</td> <td><button>Edit</button></td></tr>`
+  constructor({name, breed, sex, id}) {
+    this.name = name
+    this.breed = breed
+    this.sex = sex
+    this.id = id
+  }
+
+  element() {
+    const row = document.createElement('tr')
+    const sex = document.createElement('td')
+    sex.innerText = this.sex
+    const name = document.createElement('td')
+    name.innerText = this.name
+    const breed = document.createElement('td')
+    breed.innerText = this.breed
+    const edit = document.createElement('td')
+    const btn = document.createElement('button')
+    btn.innerText = 'Edit Dog'
+    btn.dataset.id = this.id
+    btn.addEventListener('click', DogController.handleClick)
+    edit.append(btn)
+    row.append(name, breed, sex, edit)
+    return row
   }
 }
--- src/dogController.js
+++ src/dogController.js
@@ -1,3 +1,46 @@
 class DogController {
-  
+  static init() {
+    Adapter.getDogs()
+      .then(DogController.renderDogs)
+    const form = document.querySelector('#dog-form')
+    form.addEventListener('submit', DogController.handleSubmit)
+  }
+
+  static renderDogs(dogs) {
+    dogs.forEach(DogController.renderDog)
+  }
+
+  static renderDog(dog) {
+    const table = document.querySelector('#table-body')
+    const newDog = new Dog(dog)
+    table.append(newDog.element())
+  }
+
+  static handleClick(e) {
+    const id = e.target.dataset.id
+    Adapter.getDog(id)
+      .then(DogController.populateForm)
+  }
+
+  static populateForm(dog) {
+    const newDog = new Dog(dog)
+    const form = document.querySelector('#dog-form')
+    form.dataset.id = newDog.id
+    form.name.value = newDog.name
+    form.breed.value = newDog.breed
+    form.sex.value = newDog.sex
+  }
+
+  static handleSubmit(e) {
+    e.preventDefault()
+    const data = {
+      id: e.target.dataset.id,
+      name: e.target.name.value,
+      breed: e.target.breed.value,
+      sex: e.target.sex.value
+    }
+    Adapter.editDog(data)
+    e.target.reset()
+    e.target.dataset.id = ''
+  }
 }
